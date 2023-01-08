a6b2236c552b9f79b3660c773c2b5bddb9397201
DharavathSupriya <dharavathsupriya510@gmail.com>
2023-01-09 02:32:29 +0530

    tests updated

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-undef */
 const request = require("supertest");
 const cheerio = require("cheerio");
-
 const db = require("../models/index");
 const app = require("../app");
 
@@ -15,10 +14,10 @@ function extractCsrfToken(res) {
 const login = async () => {
   await agent
     .post("/session")
-    .send({ email: "test@user.com", password: "12345678" });
+    .send({ email: "test@user.com", password: "1234567890" });
 };
 
-describe("first", () => {
+describe("Test cases", () => {
   beforeAll(async () => {
     await db.sequelize.sync({ force: true });
     server = app.listen(6000, () => {});
@@ -34,31 +33,31 @@ describe("first", () => {
     expect(1).toBe(1);
   });
 
-  test("login required", async () => {
+  test("Login required", async () => {
     const res = await agent.get("/home");
     expect(res.statusCode).toBe(302);
   });
 
-  test("admin authentication page works", async () => {
+  test("Admin authentication page(admin login)", async () => {
     const res = await agent.get("/signup");
     expect(res.statusCode).toBe(200);
     const login = await agent.get("/login");
     expect(login.statusCode).toBe(200);
   });
 
-  test("signup admin", async () => {
+  test("Signup admin", async () => {
     const signupPage = await agent.get("/signup");
     const token = extractCsrfToken(signupPage);
     const res = await agent.post("/users").send({
       name: "admin",
       email: "test@user.com",
-      password: "12345678",
+      password: "1234567890",
       _csrf: token,
     });
     expect(res.statusCode).toBe(302);
   });
 
-  test("create election", async () => {
+  test("Creates a new election", async () => {
     let count, newCount;
     const addElection = await agent.get("/elections/new");
     const token = extractCsrfToken(addElection);
